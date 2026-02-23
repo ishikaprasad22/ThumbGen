0614af1356caacfd9f31fdb4004361cb3f01cefe
ishikaprasad22 <ishikaa2207@gmail.com>
2026-02-24 00:07:45 +0530

    URL updated in CORS

--- server/server.ts
+++ server/server.ts
@@ -24,7 +24,7 @@ const app = express();
 
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://localhost:3000','https://thumbgen-two.vercel.app/'],
+    origin: ['http://localhost:5173', 'http://localhost:3000','https://thumbgen-two.vercel.app'],
     credentials: true
 }))
 
